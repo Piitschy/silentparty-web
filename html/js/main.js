60440842587802e4cd3c70aae8d78aedbfc1c812
Jan <55839828+Piitschy@users.noreply.github.com>
2021-12-05 17:41:10 +0100

    integrade Vue quick infos

--- html/js/main.js
+++ html/js/main.js
@@ -2,30 +2,34 @@ const app = Vue.createApp({
   data() {
     return {
       baseUrl: "https://cms.silentparty-hannover.de/",
-      partydate: ""
+      partydate: "coming soon",
+      quickinfos: []
     }
   },
-  methods: {
 
+  methods: {
     reformatDate(dateStr) {
       dArr = dateStr.split("-");  // ex input "2010-01-18"
       return dArr[2]+ "." +dArr[1]+ "." +dArr[0].substring(2); //ex out: "18.01.10"
     },
 
-    async loadData(url) {
-      const response = await fetch(url)
+    async loadData(subUrl) {
+      const response = await fetch(this.baseUrl+subUrl)
       json = await response.json()
       return json
     },
+
     async getPartyDate() {
-      partydate = await this.loadData(this.baseUrl+"party-date")
+      partydate = await this.loadData("party-date")
       if (!partydate.show_text) {
         return this.reformatDate(partydate.date)
       }
       return partydate.text
     }
+
   },
   async mounted() {
     this.partydate = await this.getPartyDate()
+    this.quickinfos = await this.loadData("quick-infos")
   }
 })
