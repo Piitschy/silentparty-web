42c4600de8d94c6c6db369bdf31ff956dbb61529
Jan <55839828+Piitschy@users.noreply.github.com>
2021-12-05 02:04:37 +0100

    update vue

--- html/js/main.js
+++ html/js/main.js
@@ -2,22 +2,30 @@ const app = Vue.createApp({
   data() {
     return {
       baseUrl: "https://cms.silentparty-hannover.de/",
-    }
-  },
-  computed: {
-    partydate() {
-      return this.getPartyDate()
+      partydate: ""
     }
   },
   methods: {
+
+    reformatDate(dateStr) {
+      dArr = dateStr.split("-");  // ex input "2010-01-18"
+      return dArr[2]+ "." +dArr[1]+ "." +dArr[0].substring(2); //ex out: "18.01.10"
+    },
+
     async loadData(url) {
       const response = await fetch(url)
       json = await response.json()
       return json
     },
-    getPartyDate() {
-      partydate = this.loadData(this.baseUrl+"party-date")
-      return partydate.Date || partydate.Fallback
+    async getPartyDate() {
+      partydate = await this.loadData(this.baseUrl+"party-date")
+      if (!partydate.show_text) {
+        return this.reformatDate(partydate.date)
+      }
+      return partydate.text
     }
+  },
+  async mounted() {
+    this.partydate = await this.getPartyDate()
   }
 })
